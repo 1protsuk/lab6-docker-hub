bdd3484d5449dfc6091f80d973430f762963f3a3
1protsuk <1protsukruslan@gmail.com>
2025-11-05 02:36:45 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,12 +2,10 @@ from app import app
 
 def test_hello_route():
     client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hello from Docker" in response.data
+    resp = client.get('/')
 
-    # TODO: Вкажіть ваше прізвище для перевірки
-   student_surname_bytes = b"ПРОЦУК"
+    assert resp.status_code == 200
+    assert b"Hello from Docker" in resp.data
 
-    assert student_surname_bytes in response.data
-
+    # перевіряємо прізвище (рядок -> bytes у utf-8)
+    assert "ПРОЦУК".encode("utf-8") in resp.data
